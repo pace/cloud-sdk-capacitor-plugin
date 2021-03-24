cccd1a7ee6b00db6038f30980ed96f2ece044f22
Patrick Niepel <patrick@pace.car>
2021-03-24 14:45:38 +0100

    Implement iOS' first wrapper method

---
+++ ios/Plugin/Helpers/Constants.swift
@@ -0,0 +1,19 @@
+//
+//  Constants.swift
+//  Plugin
+//
+//  Created by Patrick Niepel on 24.03.21.
+//  Copyright © 2021 Max Lynch. All rights reserved.
+//
+
+import Foundation
+
+struct Constants {
+    static let apiKey = "apiKey"
+
+    static let authenticationMode = "authenticationMode"
+    static let web = "web"
+
+    static let environment = "environment"
+    static let production = "production"
+}
