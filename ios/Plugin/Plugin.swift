544886a3ff3a89455b13727e09749933dab47b3f
Patrick Niepel <patrick@pace.car>
2021-04-13 16:53:47 +0200

    Fix threading issue

--- ios/Plugin/Plugin.swift
+++ ios/Plugin/Plugin.swift
@@ -9,6 +9,7 @@ import PACECloudSDK
  */
 @objc(CloudSDK)
 public class CloudSDK: CAPPlugin {
+    private let pluginQueue: DispatchQueue = .init(label: "plugin-cloudsdk", qos: .utility)
     private var poiKitManager: POIKit.POIKitManager?
     private var downloadTask: CancellablePOIAPIRequest?
 
@@ -17,130 +18,163 @@ public class CloudSDK: CAPPlugin {
 
     @objc
     public func setup(_ call: CAPPluginCall) {
-        guard let apiKey = call.getString(Constants.apiKey.rawValue) else {
-            call.reject("Failed setup due to a missing value for '\(Constants.apiKey.rawValue)'.")
-            return
+        pluginQueue.async { [weak self] in
+            guard let apiKey = call.getString(Constants.apiKey.rawValue) else {
+                self?.reject(call, "Failed setup due to a missing value for '\(Constants.apiKey.rawValue)'.")
+                return
+            }
+
+            let callAuthenticationMode = call.getString(Constants.authenticationMode.rawValue) ?? Constants.web.rawValue
+            let callEnvironment = call.getString(Constants.environment.rawValue) ?? Constants.production.rawValue
+
+            let authenticationMode = PACECloudSDK.AuthenticationMode(rawValue: callAuthenticationMode) ?? .web
+            let environment = PACECloudSDK.Environment(rawValue: callEnvironment) ?? .production
+
+            let configuration: PACECloudSDK.Configuration = .init(apiKey: apiKey,
+                                                                  authenticationMode: authenticationMode,
+                                                                  environment: environment)
+
+            PACECloudSDK.shared.setup(with: configuration)
+            self?.poiKitManager = POIKit.POIKitManager(environment: environment)
+
+            AppKit.shared.delegate = self
+
+            self?.resolve(call)
         }
-
-        let callAuthenticationMode = call.getString(Constants.authenticationMode.rawValue) ?? Constants.web.rawValue
-        let callEnvironment = call.getString(Constants.environment.rawValue) ?? Constants.production.rawValue
-
-        let authenticationMode = PACECloudSDK.AuthenticationMode(rawValue: callAuthenticationMode) ?? .web
-        let environment = PACECloudSDK.Environment(rawValue: callEnvironment) ?? .production
-
-        let configuration: PACECloudSDK.Configuration = .init(apiKey: apiKey,
-                                                              authenticationMode: authenticationMode,
-                                                              environment: environment)
-
-        PACECloudSDK.shared.setup(with: configuration)
-        poiKitManager = POIKit.POIKitManager(environment: environment)
-
-        AppKit.shared.delegate = self
-
-        call.resolve()
     }
 
     @objc
     public func getNearbyGasStations(_ call: CAPPluginCall) {
-        guard let radius = call.getDouble(Constants.radius.rawValue) else {
-            call.reject("Failed getNearbyGasStations due to a missing value for '\(Constants.radius.rawValue)'.")
-            return
-        }
+        pluginQueue.async { [weak self] in
+            guard let radius = call.getDouble(Constants.radius.rawValue) else {
+                self?.reject(call, "Failed getNearbyGasStations due to a missing value for '\(Constants.radius.rawValue)'.")
+                return
+            }
 
-        guard let coordinate = call.getArray(Constants.coordinate.rawValue, Double.self),
-              let lon = coordinate[safe: 0],
-              let lat = coordinate[safe: 1] else {
-            call.reject("Failed getNearbyGasStations due to a missing value for '\(Constants.coordinate.rawValue)'.")
-            return
-        }
+            guard let coordinate = call.getArray(Constants.coordinate.rawValue, Double.self),
+                  let lon = coordinate[safe: 0],
+                  let lat = coordinate[safe: 1] else {
+                self?.reject(call, "Failed getNearbyGasStations due to a missing value for '\(Constants.coordinate.rawValue)'.")
+                return
+            }
 
-        let location = CLLocationCoordinate2D(latitude: lat, longitude: lon)
+            let location = CLLocationCoordinate2D(latitude: lat, longitude: lon)
 
-        downloadTask?.cancel()
-        downloadTask = poiKitManager?.fetchPOIs(poisOfType: .gasStation, boundingBox: POIKit.BoundingBox(center: location, radius: radius), forceLoad: true) { [weak self] result in
-            switch result {
-            case .success(let stations):
-                let pluginStations: [PluginGasStation] = stations.compactMap { PluginGasStation(from: $0) }
-                self?.dispatchToMainThread(call.resolve([Constants.results.rawValue: pluginStations]))
+            self?.downloadTask?.cancel()
+            self?.downloadTask = self?.poiKitManager?.fetchPOIs(poisOfType: .gasStation, boundingBox: POIKit.BoundingBox(center: location, radius: radius), forceLoad: true) { [weak self] result in
+                switch result {
+                case .success(let stations):
+                    let pluginStations: [PluginGasStation] = stations.compactMap { PluginGasStation(from: $0) }
+                    self?.resolve(call, [Constants.results.rawValue: pluginStations])
 
-            case .failure(let error):
-                self?.dispatchToMainThread(call.reject("Failed getNearbyGasStations with error \(error.localizedDescription)"))
+                case .failure(let error):
+                    self?.reject(call, "Failed getNearbyGasStations with error \(error.localizedDescription)")
+                }
             }
         }
     }
 
     @objc
     public func isPoiInRange(_ call: CAPPluginCall) {
-        guard let poiId = call.getString(Constants.poiId.rawValue) else {
-            call.reject("Failed isPoiInRange due to a missing value for '\(Constants.poiId.rawValue)'.")
-            return
-        }
+        pluginQueue.async { [weak self] in
+            guard let poiId = call.getString(Constants.poiId.rawValue) else {
+                self?.reject(call, "Failed isPoiInRange due to a missing value for '\(Constants.poiId.rawValue)'.")
+                return
+            }
 
-        AppKit.shared.isPoiInRange(id: poiId) { result in
-            call.resolve([Constants.result.rawValue: result])
+            AppKit.shared.isPoiInRange(id: poiId) { result in
+                self?.resolve(call, [Constants.result.rawValue: result])
+            }
         }
     }
 
     @objc
     public func startApp(_ call: CAPPluginCall) {
-        guard let inputString = call.getString(Constants.url.rawValue) else {
-            call.reject("Failed startApp due to a missing value for '\(Constants.url.rawValue)'.")
-            return
+        pluginQueue.async { [weak self] in
+            guard let inputString = call.getString(Constants.url.rawValue) else {
+                self?.reject(call, "Failed startApp due to a missing value for '\(Constants.url.rawValue)'.")
+                return
+            }
+
+            self?.dispatchToMainThread {
+                let appVC: AppViewController
+
+                if let presetUrl = PACECloudSDK.URL(rawValue: inputString) {
+                    appVC = AppKit.shared.appViewController(presetUrl: presetUrl)
+                } else if URL(string: inputString) != nil {
+                    // Check if the string is a valid URL before passing it to AppKit
+                    // because the eventually resulting error callbacks
+                    // cannot be used in the plugin context
+                    appVC = AppKit.shared.appViewController(appUrl: inputString)
+                } else {
+                    self?.reject(call, "Failed startApp due to an invalid value for '\(Constants.url.rawValue)'")
+                    return
+                }
+
+                self?.presentViewController(appVC: appVC, for: call)
+            }
         }
-
-        let appVC: AppViewController
-
-        if let presetUrl = PACECloudSDK.URL(rawValue: inputString) {
-            appVC = AppKit.shared.appViewController(presetUrl: presetUrl)
-        } else if URL(string: inputString) != nil {
-            // Check if the string is a valid URL before passing it to AppKit
-            // because the eventually resulting error callbacks
-            // cannot be used in the plugin context
-            appVC = AppKit.shared.appViewController(appUrl: inputString)
-        } else {
-            call.reject("Failed startApp due to an invalid value for '\(Constants.url.rawValue)'")
-            return
-        }
-
-        presentViewController(appVC: appVC, for: call)
     }
 
     @objc
     public func startFuelingApp(_ call: CAPPluginCall) {
-        let poiId = call.getString(Constants.poiId.rawValue)
-        let appVC = AppKit.shared.appViewController(presetUrl: .fueling(id: poiId))
-        presentViewController(appVC: appVC, for: call)
+        pluginQueue.async { [weak self] in
+            let poiId = call.getString(Constants.poiId.rawValue)
+
+            self?.dispatchToMainThread {
+                let appVC = AppKit.shared.appViewController(presetUrl: .fueling(id: poiId))
+                self?.presentViewController(appVC: appVC, for: call)
+            }
+        }
     }
 
     @objc
     public func respondToEvent(_ call: CAPPluginCall) {
-        guard let eventName = call.getString(Constants.name.rawValue) else {
-            call.reject("Failed respondToEvent due to a missing value for '\(Constants.name.rawValue)'.")
-            return
-        }
+        pluginQueue.async { [weak self] in
+            guard let eventName = call.getString(Constants.name.rawValue) else {
+                self?.reject(call, "Failed respondToEvent due to a missing value for '\(Constants.name.rawValue)'.")
+                return
+            }
 
-        guard let event = PluginEvent(rawValue: eventName) else {
-            call.reject("Failed respondToEvent due to an unregistered event  - \(eventName).")
-            return
-        }
+            guard let event = PluginEvent(rawValue: eventName) else {
+                self?.reject(call, "Failed respondToEvent due to an unregistered event  - \(eventName).")
+                return
+            }
 
-        switch event {
-        case .tokenInvalid:
-            handleTokenInvalidEvent(with: call)
+            switch event {
+            case .tokenInvalid:
+                self?.handleTokenInvalidEvent(with: call)
+            }
         }
     }
 }
 
 extension CloudSDK {
+    private func resolve(_ call: CAPPluginCall, _ data: PluginResultData? = nil) {
+        dispatchToMainThread {
+            if let data = data {
+                call.resolve(data)
+            } else {
+                call.resolve()
+            }
+        }
+    }
+
+    private func reject(_ call: CAPPluginCall, _ errorMessage: String) {
+        dispatchToMainThread {
+            call.reject(errorMessage)
+        }
+    }
+
     private func presentViewController(appVC: AppViewController, for call: CAPPluginCall) {
-        dispatchToMainThread(
+        dispatchToMainThread {
             self.bridge.viewController.present(appVC, animated: true) {
                 call.resolve()
             }
-        )
+        }
     }
 
-    private func dispatchToMainThread(_ block: @autoclosure @escaping () -> Void) {
+    private func dispatchToMainThread(_ block: @escaping () -> Void) {
         DispatchQueue.main.async {
             block()
         }
@@ -150,17 +184,19 @@ extension CloudSDK {
 // MARK: - Event handling
 extension CloudSDK {
     func notify(_ event: PluginEvent, data: [String: Any] = [:]) {
-        notifyListeners(event.rawValue, data: data)
+        dispatchToMainThread { [weak self] in
+            self?.notifyListeners(event.rawValue, data: data)
+        }
     }
 
     func handleTokenInvalidEvent(with call: CAPPluginCall) {
         guard let token = call.getString(Constants.value.rawValue) else {
-            call.reject("Failed tokenInvalidEvent due to an invalid or missing token value for '\(Constants.value.rawValue)'.")
+            reject(call, "Failed tokenInvalidEvent due to an invalid or missing token value for '\(Constants.value.rawValue)'.")
             return
         }
 
         guard let id = call.getString(Constants.id.rawValue) else {
-            call.reject("Failed tokenInvalidEvent due to an invalid or missing value for '\(Constants.id.rawValue)'.")
+            reject(call, "Failed tokenInvalidEvent due to an invalid or missing value for '\(Constants.id.rawValue)'.")
             return
         }
 
@@ -168,16 +204,18 @@ extension CloudSDK {
         callback?(token)
         callbacks[id] = nil
 
-        call.resolve()
+        resolve(call)
     }
 }
 
 // MARK: - AppKitDelegate
 extension CloudSDK: AppKitDelegate {
     public func tokenInvalid(completion: @escaping ((String) -> Void)) {
-        let id = UUID().uuidString
-        callbacks[id] = completion
-        notify(.tokenInvalid, data: [Constants.id.rawValue: id])
+        pluginQueue.async { [weak self] in
+            let id = UUID().uuidString
+            self?.callbacks[id] = completion
+            self?.notify(.tokenInvalid, data: [Constants.id.rawValue: id])
+        }
     }
 
     public func didFail(with error: AppKit.AppError) {}
