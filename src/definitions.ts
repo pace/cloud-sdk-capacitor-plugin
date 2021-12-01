51b20110b85dbc49f1a26cb0c85db8fedaea49b5
Martin Dinh <martin.dinh@pace.car>
2021-12-01 18:01:54 +0100

    fix: Event and native token handling

--- src/definitions.ts
+++ src/definitions.ts
@@ -1,21 +1,24 @@
+import {PluginListenerHandle} from "@capacitor/core/dist/esm/definitions";
+import {ListenerCallback} from "@capacitor/core";
+​
 declare module '@capacitor/core' {
   interface PluginRegistry {
     CloudSDK: CloudSDKPlugin;
   }
 }
-
+​
 export enum AuthenticationMode {
   WEB = 'web',
   NATIVE = 'native',
 }
-
+​
 export enum Environment {
   PRODUCTION = 'production',
   DEVELOPMENT = 'development',
   STAGE = 'stage',
   SANDBOX = 'sandbox',
 }
-
+​
 export enum OpeningHourDay {
   MONDAY = 'monday',
   TUESDAY = 'tuesday',
@@ -25,11 +28,11 @@ export enum OpeningHourDay {
   SATURDAY = 'saturday',
   SUNDAY = 'sunday',
 }
-
+​
 export enum FuelCurrency {
   EUR = 'EUR',
 }
-
+​
 export enum FuelType {
   RON98 = 'ron98',
   RON98E5 = 'ron98e5',
@@ -53,30 +56,30 @@ export enum FuelType {
   TRUCKLPG = 'truckLpg',
   HEATINGOIL = 'heatingOil',
 }
-
+​
 export enum FuelPriceUnit {
   LITRE = 'L',
 }
-
+​
 export interface Configuration {
   /**
    * Use this property to provide the plugin with your PACE SDK API Key
    */
   apiKey: string;
-
+​
   /**
    * If you use native logins, then this should be set to 'AuthenticationMode.NATIVE'.
    * Defaults to 'AuthenticationMode.WEB'
    */
   authenticationMode?: AuthenticationMode;
-
+​
   /**
    * Set your environment to 'Environment.STAGE' or 'Environment.SANDBOX' while developing or running tests.
    * Defaults to 'Environment.PRODUCTION'
    */
   environment?: Environment;
 }
-
+​
 export interface Address {
   countryCode?: string;
   city?: string;
@@ -86,12 +89,12 @@ export interface Address {
   street?: string;
   houseNumber?: string;
 }
-
+​
 export interface OpeningHour {
   day: OpeningHourDay;
   hours: [number, number][];
 }
-
+​
 export interface FuelPrice {
   fuelType: FuelType;
   productName: string;
@@ -101,71 +104,71 @@ export interface FuelPrice {
   currency: FuelCurrency;
   updated: number;
 }
-
+​
 export interface GasStation {
   id: string;
   name: string;
   address?: Address;
-
+​
   /**
    * A set of coordinates, as [lng, lat]
    */
   coordinates: [number, number];
-
+​
   /**
    * The opening hours for the Gas Station
    */
   openingHours: OpeningHour[];
-
+​
   /**
    * The available fuel prices
    */
   fuelPrices: FuelPrice[];
-
+​
   /**
    * Indicates if the gasStations supports Connected Fueling
    */
   isConnectedFuelingAvailable: boolean;
   lastUpdated: Date;
 }
-
+​
 export interface EventResponse {
   /**
    * The `id` must be the same as from the received event
    */
   id: string;
-
+​
   /**
    * The `name` must be the same as from the received event
    */
   name: string;
-
+​
   /**
    * In case the `value` is an object, make sure to stringify it first
    */
   value: string;
 }
-
+​
 export enum PresetUrl {
   PACE_ID = 'paceID',
   PAYMENT = 'payment',
   TRANSACTIONS = 'transactions',
 }
-
+​
 export interface CloudSDKPlugin {
   /**
    * Setup the plugin
    * @param config is the configuration provided to this method
    */
   setup(config: Configuration): Promise<boolean>;
-
+​
   /**
    * Method that can to be called whenever the client wants
    * to communicate to the plugin in regards to a received event
    * @param response is the response to a given event
    */
    respondToEvent(response: EventResponse): Promise<boolean>;
-
+​
   /**
    * Returns a list of gasStations based on a given coordinate
    * @param coordinate is the coordinate to search from, as [lng, lat]
@@ -175,7 +178,7 @@ export interface CloudSDKPlugin {
     coordinate: [number, number];
     radius: number;
   }): Promise<{ results: GasStation[] }>;
-
+​
   /**
    * Check if there is a App for the given GasStation Id at the current location
    * @param poiId is the Id of a GasStation
@@ -185,16 +188,18 @@ export interface CloudSDKPlugin {
     poiId: string;
     coordinate: [number, number];
   }): Promise<{ result: boolean }>;
-
+​
   /**
    * Start an App via a url
    * @param url is the appStartUrl from an App, or one of predefined presetUrls
    */
   startApp(options: { url: string | PresetUrl }): Promise<boolean>;
-
+​
   /**
    * Start an App for a given poiId
    * @param url is the Id of a GasStation
    */
   startFuelingApp(options: { poiId: string }): Promise<boolean>;
+​
+  addListener(eventName: string, listenerFunc: ListenerCallback): PluginListenerHandle;
 }
